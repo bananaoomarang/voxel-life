1cd3a069eb027097d77cea9c3d1914bf01fcc26f
Milo Mordaunt <milomord@gmail.com>
2013-10-12 19:22:07 +0100

    Seems to work

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function Life(opts) {
                     break;
                 case 1:
                     this.grid[x][y] = ALIVE;
+                    this.game.setBlock([x, y + 1, -5], 1);
                     break;
                 default:
                     console.log('WTF maths just broke call Milo');
@@ -41,7 +42,6 @@ Life.prototype.tick = function(dt) {
     this.timer -= dt;
     if(this.timer <= 0) {
         this.timer = this.speed;
-        console.log(this.grid);
         this.update();
     }
 }
